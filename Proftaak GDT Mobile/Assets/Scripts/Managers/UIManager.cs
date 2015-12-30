c59469ace009a076c9ee771f614cfc1100e29131
SHEePYTaGGeRNeP <sheepytaggernep@gmail.com>
2015-12-30 22:47:24 +0100

    Je kan nu ook upgrade canvas openen en sluiten.

---
+++ Proftaak GDT Mobile/Assets/Scripts/Managers/UIManager.cs
@@ -0,0 +1,33 @@
+﻿namespace Assets.Scripts.Managers
+{
+    using UnityEngine;
+    internal class UIManager : MonoBehaviour
+    {
+        [SerializeField]
+        private Canvas UpgradeCanvas;
+
+        [SerializeField]
+        private Canvas UICanvas;
+
+        public static UIManager Instance;
+
+
+        public void GoToUpgradeCanvas()
+        {
+            this.UpgradeCanvas.gameObject.SetActive(true);
+            this.UICanvas.gameObject.SetActive(false);
+        }
+
+        public void LeaveUpgradeCanvas()
+        {
+            this.UICanvas.gameObject.SetActive(true);
+            this.UpgradeCanvas.gameObject.SetActive(false);
+        }
+
+        private void Awake()
+        {
+            if (Instance == null)
+                Instance = this;
+        }
+    }
+}
