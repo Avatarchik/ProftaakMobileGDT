7f33c1cb374e057a1321199bc811998678973c0d
SHEePYTaGGeRNeP <sheepytaggernep@gmail.com>
2016-01-03 14:35:47 +0100

    Null reference exception opgelost + volgers groeien nu ook met 5% van je current followers voor die groep. Random Events nog niet echt iets mee gedaan.

--- Proftaak GDT Mobile/Assets/Scripts/Managers/FollowerManager.cs
+++ Proftaak GDT Mobile/Assets/Scripts/Managers/FollowerManager.cs
@@ -9,7 +9,7 @@
 
     public class FollowerManager : MonoBehaviour
     {
-        public static FollowerManager Instance = new FollowerManager();
+        public static FollowerManager Instance;
 
 
         public List<FollowerGroup> FollowerGroups;
@@ -21,11 +21,16 @@
 
         [SerializeField]
         private FollowerGroup _followerGroupPrefab;
+
+        [SerializeField]
+        private Canvas _followersCanvas;
         
 
         // ReSharper disable once UnusedMember.Local
         private void Awake()
         {
+            if (Instance == null)
+                Instance = this;
             this.InvokeRepeating("IncreaseFollowers", 0f, Random.Range(0.25f, 0.5f));
         }
 
@@ -40,13 +45,17 @@
         {
             // TODO: Fix magic numbers
             foreach (FollowerGroup fg in this.FollowerGroups.Where(fg => Random.Range(0, 4) > 2))
-                fg.Followers += Random.Range(5, 20);
+            {
+                fg.Followers += Random.Range(5, 20) + (int)(fg.Followers * 0.05);
+            }
         }
 
         public void CreateNewFollowerGroup(Vector2 pos, int startFollowers)
         {
             FollowerGroup newGroup = GameObject.Instantiate(this._followerGroupPrefab);
-            newGroup.GetComponent<RectTransform>().sizeDelta = pos;
+            newGroup.transform.SetParent(this._followersCanvas.transform);
+            //newGroup.GetComponent<RectTransform>().sizeDelta = new Vector2(100,100);
+            newGroup.GetComponent<RectTransform>().position = pos;
             newGroup.Followers = startFollowers;
             this.FollowerGroups.Add(newGroup);
             
