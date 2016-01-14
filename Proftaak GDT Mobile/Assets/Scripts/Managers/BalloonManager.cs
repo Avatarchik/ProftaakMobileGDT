90e1a0c90009b741272546d0bfea4e71e8276598
Lorenzo <sheepytaggernep@gmail.com>
2016-01-14 11:10:08 +0100

    Balloons nieuwe canvas + clickbaar.

--- Proftaak GDT Mobile/Assets/Scripts/Managers/BalloonManager.cs
+++ Proftaak GDT Mobile/Assets/Scripts/Managers/BalloonManager.cs
@@ -16,7 +16,7 @@
         private LightbulbBalloon _firstLightbulbBalloon;
         [SerializeField]
         private RandomEventBalloon _firstRandomEventBalloon;
-        
+
 
         // ReSharper disable once UnusedMember.Local
         private void Awake()
@@ -28,6 +28,27 @@
             this.LightBulbBalloons.Add(this._firstLightbulbBalloon);
             this.RandomEventsBalloons.Add(this._firstRandomEventBalloon);
             this._firstRandomEventBalloon.gameObject.SetActive(true);
+        }
+
+        private void Update()
+        {
+            Ray ray = new Ray(Vector3.back, Vector3.down);
+            if (Input.touchCount > 0)
+                ray = Camera.main.ScreenPointToRay(Input.GetTouch(0).position);
+            else if (Input.GetMouseButtonDown(0))
+                ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (ray.origin == Vector3.back && ray.direction == Vector3.down) return;
+
+            RaycastHit2D[] hits = Physics2D.RaycastAll(ray.origin, new Vector3(0, 0, 2000f));
+            //Debug.DrawRay(ray.origin, new Vector3(0, 0, 1500f), Color.green, 3f);
+            foreach (RaycastHit2D hit in hits)
+            {
+                if (hit.transform.GetComponent<LightbulbBalloon>() != null)
+                    hit.transform.GetComponent<LightbulbBalloon>().OnButtonClicked();
+                else  if (hit.transform.GetComponent<RandomEventBalloon>() != null)
+                    hit.transform.GetComponent<RandomEventBalloon>().OnButtonClicked();
+
+            }
         }
 
     }
