87fd6fdd1d7a1d7e1967d2187210ef8419dbe03a
jorisvandongen <jorisoworst@live.nl>
2016-01-28 13:36:33 +0100

    Ted introductie in idee menu

---
+++ Proftaak GDT Mobile/Assets/Scripts/IdeaMenuScript.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class IdeaMenuScript : MonoBehaviour {
+
+	[SerializeField]
+	private GameObject _tutorialPanel;
+
+	[SerializeField]
+	private Text _titleText;
+
+	[SerializeField]
+	private Text _descriptionText;
+
+	private int _stepCounter = 0;
+
+	public void ButtonClick()
+	{
+		this._stepCounter++;
+
+		if (this._stepCounter == 1) {
+			this._titleText.text = "Doel";
+			this._descriptionText.text = "Je hebt een idee en je wil dit verspreiden. Met genoeg volgers mag je op het TEDxVeghel evenement spreken. Je kunt hier je naam, de naam van je idee en de categorie van je idee invullen.";
+		} else {
+			this._tutorialPanel.SetActive (false);
+		}
+	}
+}
