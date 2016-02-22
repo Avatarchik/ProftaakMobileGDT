cce9c7f28bbf3ef04a0d3393c0e06c1fe9fa9e38
SHEePYTaGGeRNeP <sheepytaggernep@gmail.com>
2016-02-22 21:32:36 +0100

    Fixed IdeaScene + Added ErrorMessage bij niet invullen van ideenaam etc. TED videos beginnen in titel met TEDvideo: A roadmap to end aging".

--- Proftaak GDT Mobile/Assets/Scripts/IdeaScene/ButtonHandler.cs
+++ Proftaak GDT Mobile/Assets/Scripts/IdeaScene/ButtonHandler.cs
@@ -15,27 +15,50 @@ namespace Assets.Scripts.IdeaScene
         private InputField IdeeNaamTb;
         [SerializeField]
         private Dropdown CategorieDropDrown;
-        
-        
+
+        [SerializeField]
+        private GameObject FoutmeldingPanel;
+
+        [SerializeField]
+        private Text ErrorText;
+
         public void StartButtonClicked()
         {
             String captiontext = this.CategorieDropDrown.captionText.text;
             Debug.Log(captiontext);
 
-            if(!this.SpelerNaamTb.text.IsNullEmptyOrWhitespace() && !this.IdeeNaamTb.text.IsNullEmptyOrWhitespace() && this.CategorieDropDrown.captionText.text != "Kies een categorie")
+            if (this.SpelerNaamTb.text.IsNullEmptyOrWhitespace())
             {
-                Player.Instance = new Player();
-                Player.Instance.PlayerName= this.SpelerNaamTb.text;
-                Player.Instance.IdeaName = this.IdeeNaamTb.text;
-                Player.Instance.Category = (IdeaCategory)Enum.Parse(typeof(IdeaCategory), this.CategorieDropDrown.captionText.text.Replace(" ","_"));
-                SceneManager.LoadScene("Nederland");
+                this.ShowFoutmeldingCanvas("De Spelernaam mag niet leeg zijn");
+                return;
             }
-            else
+            if (this.IdeeNaamTb.text.IsNullEmptyOrWhitespace())
             {
-                Debug.Log("OH NOO");
+                this.ShowFoutmeldingCanvas("De naam van je idee mag niet leeg zijn");
+                return;
             }
+            if (this.CategorieDropDrown.captionText.text == "Kies een categorie")
+            {
+                this.ShowFoutmeldingCanvas("Kies een categorie");
+                return;
+            }
+            Player.Instance = new Player();
+            Player.Instance.PlayerName = this.SpelerNaamTb.text;
+            Player.Instance.IdeaName = this.IdeeNaamTb.text;
+            Player.Instance.Category = (IdeaCategory)Enum.Parse(typeof(IdeaCategory), this.CategorieDropDrown.captionText.text.Replace(" ", "_"));
+            SceneManager.LoadScene("Nederland");
+        }
 
-        } 
+        private void ShowFoutmeldingCanvas(string text)
+        {
+            this.ErrorText.text = text;
+            this.FoutmeldingPanel.SetActive(true);
+        }
+
+        public void ButtonCloseClicked()
+        {
+            this.FoutmeldingPanel.SetActive(false);
+        }
 
         public void BackButtonClicked()
         {
