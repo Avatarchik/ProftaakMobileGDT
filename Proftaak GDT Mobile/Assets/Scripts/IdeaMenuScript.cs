5838b49eb4d6730c3261847ebb2d542debeba862
SHEePYTaGGeRNeP <sheepytaggernep@gmail.com>
2016-03-15 20:29:33 +0100

    IdeaMenu scales better. Somehow a change in Nederlandscene?

--- Proftaak GDT Mobile/Assets/Scripts/IdeaMenuScript.cs
+++ Proftaak GDT Mobile/Assets/Scripts/IdeaMenuScript.cs
@@ -1,29 +1,36 @@
 ﻿using UnityEngine;
-using System.Collections;
 using UnityEngine.UI;
 
-public class IdeaMenuScript : MonoBehaviour {
+public class IdeaMenuScript : MonoBehaviour
+{
 
-	[SerializeField]
-	private GameObject _tutorialPanel;
+    [SerializeField]
+    private GameObject _tutorialPanel;
 
-	[SerializeField]
-	private Text _titleText;
+    [SerializeField]
+    private GameObject _tutorialBackground;
 
-	[SerializeField]
-	private Text _descriptionText;
+    [SerializeField]
+    private Text _titleText;
 
-	private int _stepCounter = 0;
+    [SerializeField]
+    private Text _descriptionText;
 
-	public void ButtonClick()
-	{
-		this._stepCounter++;
+    private int _stepCounter = 0;
 
-		if (this._stepCounter == 1) {
-			this._titleText.text = "Doel";
-			this._descriptionText.text = "Je hebt een idee en je wil dit verspreiden. Met genoeg volgers mag je op het TEDxVeghel evenement spreken. Je kunt hier je naam, de naam van je idee en de categorie van je idee invullen.";
-		} else {
-			this._tutorialPanel.SetActive (false);
-		}
-	}
+    public void ButtonClick()
+    {
+        this._stepCounter++;
+
+        if (this._stepCounter == 1)
+        {
+            this._titleText.text = "Doel";
+            this._descriptionText.text = "Je hebt een idee en je wil dit verspreiden. Met genoeg volgers mag je op het TEDxVeghel evenement spreken. Je kunt hier je naam, de naam van je idee en de categorie van je idee invullen.";
+        }
+        else
+        {
+            this._tutorialPanel.SetActive(false);
+            this._tutorialBackground.SetActive(false);
+        }
+    }
 }
